fe763adcc7f8dd9bfa8080c557f22b289ed6c928
Dimensionexpert <dimensionexpert2725@gmail.com>
2025-12-28 21:18:33 +0530

    Modified the ga.fish

--- fish/.config/fish/functions/ga.fish
+++ fish/.config/fish/functions/ga.fish
@@ -1,3 +1,23 @@
 function ga
-    git add .
+    if test (count $argv) -eq 0
+        echo "ga: staging all changes"
+        git add .
+        return
+    end
+
+    set valid_files
+
+    for file in $argv
+        if test -e "$file"
+            set valid_files $valid_files $file
+        else
+            echo "ga: warning — '$file' not found"
+        end
+    end
+
+    if test (count $valid_files) -gt 0
+        git add $valid_files
+    else
+        echo "ga: nothing to add"
+    end
 end
